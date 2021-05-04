93f786ccd8cb4cd4fd20511085e6f8003d241955
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-03 17:14:28 -0700

    adding hero image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Hero from "./components/Hero"
 //import pages and components
-import './App.css';
+
 
 function App() {
   return (
     <Router>
       <div>
+        <Hero backgroundImage="./teamwork.jpg" />
 
       </div>
     </Router>
