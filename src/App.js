871133afd7f1489a294a6694693b4e996d5e33df
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-04 15:34:12 -0700

    adding react-bootstrap for modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import Hero from "./components/Hero"
 //import pages and components
 
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Hero backgroundImage="./teamwork.jpg">
-          <h1>My Employee Dashboard</h1>
-        </Hero>
+  const [showModal, setShowModal] = useState(false);
 
-      </div>
-    </Router>
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  return (
+    <div>
+      <Hero backgroundImage="./teamwork.jpg">
+        <h1>My Employee Dashboard</h1>
+      </Hero>
+
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+      </Button>
+
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+          </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+          </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </div>
   );
 }
 
