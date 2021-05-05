7863d65fa84b57c10c7984d31d6659db988d09fa
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-04 23:55:54 -0700

    do not need separate modal component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import EmployeeContainer from "./components/EmployeeContainer";
 import Hero from "./components/Hero"
 //import pages and components
 
@@ -15,23 +16,23 @@ function App() {
       <Hero backgroundImage="./teamwork.jpg">
         <h1>My Employee Dashboard</h1>
       </Hero>
-
+      <EmployeeContainer />
       <>
         <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
+          Employee Names will be modal triggers
       </Button>
 
         <Modal show={showModal} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Employee Name Here</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>All of the employee info</Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
           </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
+            <Button variant="danger" onClick={handleClose}>
+              Remove Employee
           </Button>
           </Modal.Footer>
         </Modal>
