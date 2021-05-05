2957ef1c0c4fe4178b87c7b2c3c32a2e46a1de05
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-04 23:55:38 -0700

    creating employee table

---
+++ src/components/EmployeeContainer/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import getEmployees from "../../utils/API";
+
+class EmployeeContainer extends Component {
+    state = {
+        employees: [],
+        sort: ""
+    };
+
+    componentDidMount = () => {
+        getEmployees()
+            .then((res) => {
+                const employees = res.data.results.map((employee, i) => ({
+                    ...employee,
+                    id: i + 1
+                }));
+                this.setState({ employees })
+            })
+            .catch(err => console.log(err));
+    };
+
+    render() {
+
+        return (
+            <div className="container-fluid">
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Employee ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.name.first}</td>
+                                <td>{employee.name.last}</td>
+                                <td>{employee.phone}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+}
+
+export default EmployeeContainer
