b552443738bf868c8c41f0df72cfe8c15a3a4454
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-06 16:13:39 -0700

    adding more effects and state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EmployeeContainer from "./components/EmployeeContainer";
 import Hero from "./components/Hero";
 import EmployeeModal from "./components/EmployeeModal";
+import Search from "./components/Search";
+import getEmployees from "./utils/API";
 
 
 
 function App() {
   const [employee, setShowModal] = useState();
+  const [allEmployees, setAllEmployees] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
   const handleClose = () => setShowModal(undefined);
   const handleShow = (employee) => setShowModal(employee);
 
 
+  useEffect(() => {
+    getEmployees()
+      .then((res) => {
+        const employees = res.data.results.map((employee, i) => ({
+          ...employee,
+          id: i + 1
+        }));
+        setAllEmployees(employees)
+        setFilteredEmployees(employees)
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    setFilteredEmployees(allEmployees.filter((employee) => {
+      employee.name.first.includes(searchValue) || employee.name.last.includes(searchValue) || employee.email.includes(searchValue)
+    }))
+  }, [searchValue]);
+
   return (
-    <div>
+    <div className="container-fluid">
       <Hero backgroundImage="./images/teamwork.jpg">
         <h1>My Employee Dashboard</h1>
       </Hero>
-
-      <EmployeeContainer showModal={handleShow} />
-
-      <EmployeeModal employee={employee} handleClose={handleClose} />
-
+      <div className="row">
+        <div className="col-3">
+          <Search search={searchValue} handleInputChange={(e) => setSearchValue(e.target.value)} />
+        </div>
+        <div className="col-9">
+          <EmployeeContainer showModal={handleShow} allEmployees={searchValue.length ? filteredEmployees : allEmployees} />
+        </div>
+        <EmployeeModal employee={employee} handleClose={handleClose} />
+      </div>
     </div>
   );
 }
